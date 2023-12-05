079f15d545653ca794c1f13df19feb295ec1a54a
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 04:00:26 +0700

    menambahkan function SiswaTopAppBar

--- app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
@@ -1,17 +1,41 @@
 package com.example.praktikum7.navigasi
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.praktikum7.ui.theme.halaman.EntrySiswaScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior = null,
+    navigateUp: ()-> Unit = {}
+){
+    
+}
 @Composable
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
-    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ){
-        
+    NavHost(
+        navController = navController, startDestination = DestinasiHome.route, modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
     }
 }
