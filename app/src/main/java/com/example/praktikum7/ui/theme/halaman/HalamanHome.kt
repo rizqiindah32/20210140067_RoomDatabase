1a9b5368e37557aae5af290841019ea1818bee67
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 03:02:09 +0700

    menambahkan outlinedtextfield detailsiswa.nama

--- app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanHome.kt
@@ -2,10 +2,14 @@ package com.example.praktikum7.ui.theme.halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import com.example.praktikum7.R
 import com.example.praktikum7.model.DetailSiswa
 
@@ -21,6 +25,13 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ){
-
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = {onValueChange(detailSiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.nama))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+            )
     }
 }
