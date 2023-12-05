5a6bd212a895736723404c68772bac4526523e0f
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 03:45:45 +0700

    membuat file PetaNavigasi.kt

--- app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
@@ -1,2 +1,14 @@
 package com.example.praktikum7.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    
+}
