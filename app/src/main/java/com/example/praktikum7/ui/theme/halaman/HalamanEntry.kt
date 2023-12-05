ea617c423f0cb8ce3a3cfc833e8a33790950c31f
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 03:25:23 +0700

    menbambahkan button pada function EntrySiswaBody

--- app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanEntry.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,6 +39,9 @@ fun EntrySiswaBody(
         )
         Button(
             onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
             ) {
 
         }
