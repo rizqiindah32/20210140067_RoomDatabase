83053fe39a7f322fc982bcf4a0fb4d47d0759269
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 03:49:42 +0700

    menambahkan method

--- app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
@@ -4,11 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
-    
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ){
+        
+    }
 }
