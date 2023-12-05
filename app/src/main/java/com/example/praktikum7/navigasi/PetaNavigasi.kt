ee0417a36c0da85af3f8a8622f8ad0db9457bb25
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 04:04:21 +0700

    menambahkan function SiswaApp

--- app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
@@ -1,15 +1,28 @@
 package com.example.praktikum7.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum7.R
 import com.example.praktikum7.ui.theme.halaman.EntrySiswaScreen
 
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
@@ -19,7 +32,20 @@ fun SiswaTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior = null,
     navigateUp: ()-> Unit = {}
 ){
-    
+    CenterAlignedTopAppBar(title = { Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon( 
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                    
+                }
+            }
+        })
 }
 @Composable
 fun HostNavigasi(
