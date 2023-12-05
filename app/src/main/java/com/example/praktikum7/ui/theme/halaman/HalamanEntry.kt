6ed75f71ccf6bcb3e4ca995fd7f85d595c6d0bc4
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 03:32:10 +0700

    menambahkan val coroutinescope dan val scrollbehavior

--- app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanEntry.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -30,7 +32,8 @@ fun EntrySiswaScreen(
     modifier: Modifier = Modifier,
     viewmodel: EntryViewmodel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-    
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
 @Composable
 fun EntrySiswaBody(
