af3ec22455640298963f27a10af85b69285c3bc1
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-06 19:21:23 +0700

    membuat function pada HalamanHome.kt

--- app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum7/navigasi/PetaNavigasi.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
--- app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/praktikum7/ui/theme/halaman/HalamanHome.kt
@@ -1,2 +1,13 @@
 package com.example.praktikum7.ui.theme.halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.praktikum7.data.Siswa
+
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+){
+    
+}
